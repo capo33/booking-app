e20e19ff129761b09009defff4f7c0a557be570a
Mohamed <s2000588@edu.bc.fi>
2024-04-15 18:51:55 +0300

    create detail section component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,8 +7,12 @@ import {
 import Layout from './layout/Layout';
 import Register from './pages/Register';
 import SignIn from './pages/SignIn';
+import AddHotel from './pages/AddHotel';
+import { useAppContext } from './hooks/useAppContext';
 
 function App() {
+  const { isLoggedIn } = useAppContext();
+
   return (
     <Router>
       <Routes>
@@ -44,6 +48,16 @@ function App() {
             </Layout>
           }
         />
+        {isLoggedIn && <>
+          <Route
+            path='/add-hotel'
+            element={
+              <Layout>
+                <AddHotel />
+              </Layout>
+            }
+          />
+        </>}
         <Route path='*' element={<Navigate to={'/'} />} />
       </Routes>
     </Router>
---
+++ frontend/src/forms/ManageHotelForm/TypeSection.tsx
@@ -0,0 +1,11 @@
+import { hotelFacilities } from '../../config/hotel-options-config'
+
+const TypeSection = () => {
+  console.log(hotelFacilities);
+  
+  return (
+    <div>TypeSection</div>
+  )
+}
+
+export default TypeSection
