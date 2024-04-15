b0eceb6bd7f65e96c33989ffb58e4468bf846fb2
Mohamed <s2000588@edu.bc.fi>
2024-04-15 19:15:26 +0300

    create TypeSection component

--- frontend/src/forms/ManageHotelForm/TypeSection.tsx
+++ frontend/src/forms/ManageHotelForm/TypeSection.tsx
@@ -1,10 +1,37 @@
-import { hotelFacilities } from '../../config/hotel-options-config'
+import { useFormContext } from 'react-hook-form'
+import { hotelTypes } from '../../config/hotel-options-config'
+import { HotelFormData } from './ManageHotelForm'
 
 const TypeSection = () => {
-  console.log(hotelFacilities);
+  const { register, watch, formState: { errors } } = useFormContext<HotelFormData>()
+
+  const typeWatch = watch('type')
   
   return (
-    <div>TypeSection</div>
+    <div>
+      <h2 className='text-2xl font-bold mb-3'>Type</h2>
+      <div className='grid grid-cols-5 gap-2'>
+        {hotelTypes.map((type) => (
+          <label key={type} className={
+            `cursor-pointer text-sm rounded-full px-4 py-2 font-semibold ${typeWatch === type ? 'bg-blue-500' : 'bg-gray-300'
+            }`
+          }>
+            <input
+              type='radio'
+              value={type}
+              className='hidden'
+              {...register('type', { required: 'This field is required' })}
+            ></input>
+            <span>
+              {type}
+            </span>
+          </label>
+        ))}
+      </div>
+      {errors.type && (
+        <span className='text-red-500'>{errors.type.message}</span>
+      )}
+    </div>
   )
 }
 
