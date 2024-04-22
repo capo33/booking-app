d93096c2cdda9d8fe1254a2d5da3e73e6a31fd6d
Mohamed <s2000588@edu.bc.fi>
2024-04-22 23:22:41 +0300

    update ManageHotelForm component

--- frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
+++ frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
@@ -19,17 +19,60 @@ export type HotelFormData = {
   childCount: number;
 }
 
-const ManageHotelForm = () => {
+type ManageHotelFormProps = {
+  onSave: (hotelFormData: FormData) => void;
+  isLoading: boolean;
+}
+
+const ManageHotelForm = ({onSave, isLoading}:ManageHotelFormProps) => {
   const formMethods = useForm<HotelFormData>();
+
+  const { handleSubmit } = formMethods
+
+  const onSubmit = handleSubmit((formDataJson: HotelFormData) => {
+    const formData = new FormData();
+    formData.append('name', formDataJson.name);
+    formData.append('city', formDataJson.city);
+    formData.append('country', formDataJson.country);
+    formData.append('description', formDataJson.description);
+    formData.append('type', formDataJson.type);
+    formData.append('pricePerNight', formDataJson.pricePerNight.toString());
+    formData.append('starRating', formDataJson.starRating.toString());
+    formData.append('adultCount', formDataJson.adultCount.toString());
+    formData.append('childCount', formDataJson.childCount.toString());
+
+    // formData.append('facilities', JSON.stringify(formDataJson.facilities));
+    // for (let i = 0; i < formDataJson.imageFiles.length; i++) {
+    //   formData.append('images', formDataJson.imageFiles[i]);
+    // }
+
+    formDataJson.facilities.forEach((facility, index) => {
+      formData.append(`facilities[${index}]`, facility)
+    })
+
+    Array.from(formDataJson.imageFiles).forEach((imageFile) => {
+      formData.append(`imageFiles`, imageFile);
+    })
+
+    onSave(formData)
+  });
 
   return (
     <FormProvider {...formMethods}>
-      <form className="flex flex-col gap-10">
+      <form className="flex flex-col gap-10" onSubmit={onSubmit}>
         <DetailsSection />
         <TypeSection />
         <FacilitiesSection />
         <GuestSection />
         <ImagesSection />
+        <span className="flex justify-end">
+          <button
+            disabled={isLoading}
+            type="submit"
+            className="px-6 py-2 bg-blue-600 text-white rounded hover:bg-blue-500 fonHt-bold text-xl disabled:bg-gray-500">
+            {isLoading ? 'Loading...' : 'Save'}
+            </button>
+        </span>
       </form>
     </FormProvider>
   )
--- frontend/src/pages/AddHotel.tsx
+++ frontend/src/pages/AddHotel.tsx
@@ -1,8 +1,23 @@
+import { useMutation } from "react-query"
 import ManageHotelForm from "../forms/ManageHotelForm/ManageHotelForm"
-
+import { useAppContext } from "../hooks/useAppContext"
+import * as apiClient from "../api-client"
 const AddHotel = () => {
+  const { showToast } = useAppContext();
+  const { mutate, isLoading } = useMutation(apiClient.addMyHotel, {
+    onSuccess: () => {
+      showToast({ message: 'Hotel added successfully', type: 'SUCCESS' })
+    },
+    onError: () => {
+      showToast({ message: 'Failed to add hotel', type: 'ERROR' })
+    }
+  })
+
+  const handleSave = (hotelFormData: FormData): void => { 
+    mutate(hotelFormData)
+  }
   return (
-    <ManageHotelForm />
+    <ManageHotelForm onSave={handleSave} isLoading={isLoading} />
   )
 }
 
